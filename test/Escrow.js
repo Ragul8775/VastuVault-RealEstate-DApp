49e25da5faae55c9e6c04a64992dbfac7633c013
Ragul8775 <rb7439@srmist.edu.in>
2024-03-01 22:39:53 +0530

    Added function to test result

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,10 @@ const tokens = (n) => {
 };
 
 describe("Escrow", () => {
-  let buyer, seller;
-  let realEstate;
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
   it("save the address", async () => {
-    [buyer, seller] = await ethers.getSigners();
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
 
     //Deploy RealEstate
     const RealEstate = await ethers.getContractFactory("RealEstate");
@@ -23,6 +23,19 @@ describe("Escrow", () => {
         "https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/1.json"
       );
     await transaction.wait();
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
+
+    let result = await escrow.nftAddress();
+    expect(result).to.equal(realEstate.address);
+
+    result = await escrow.seller();
+    expect(result).to.equal(seller.address);
 
     /* ("https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/1.json") */
   });
